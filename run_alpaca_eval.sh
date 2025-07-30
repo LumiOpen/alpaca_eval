2faf091fbd1e25c085d0e1da68fc75455a90b1bb
ezosa <elaine.zosa@gmail.com>
2025-07-30 10:54:29 +0300

    extract model_name from path

--- run_alpaca_eval.sh
+++ run_alpaca_eval.sh
@@ -1,38 +1,18 @@
-#!/bin/bash
-#SBATCH --job-name=alpacaeval  # Job name
-#SBATCH --nodes=1
-#SBATCH --partition=dev-g
-#SBATCH --time=01:00:00
-#SBATCH --ntasks-per-node=8
-#SBATCH --cpus-per-task=7
-#SBATCH --exclusive=user
-#SBATCH --hint=nomultithread
-#SBATCH --gpus-per-node=mi250:8
-#SBATCH --account=project_462000353
-#SBATCH --mem=480G
-#SBATCH --output=logs/%j.out
-#SBATCH --error=logs/%j.err
-
-module use /appl/local/csc/modulefiles/
-module load pytorch
-source /scratch/project_462000353/zosaelai2/.alpaca_venv/bin/activate
-
-export PYTHONPATH="/scratch/project_462000353/zosaelai2"
-export HF_HOME="/scratch/project_462000353/hf_cache"
-
-# Ensure at least 3 arguments are provided
-if [ "$#" -lt 3 ]; then
-  echo "Usage: $0 <model_name> <model_path> <lang1> [<lang2> ...]"
+# Ensure at least 2 arguments are provided
+if [ "$#" -lt 2 ]; then
+  echo "Usage: $0 <model_path> <lang1> [<lang2> ...]"
   exit 1
 fi
 
 # First two arguments
 model_path="$1"
-model_name="$2"
-shift 2  # Remove first two args from the list
+model_name=$(basename "$model_path")
+echo "model_name: $model_name"
+shift 1  # Remove first two args from the list
 
 # Remaining arguments are language codes
 lang_codes=("$@")  # Array of language codes
+echo "langs to eval: $lang_codes"
 
 output_dir="./src/alpaca_eval/models_configs/$model_name"
 yaml_file="$output_dir/configs.yaml"
@@ -84,6 +64,7 @@ echo "DONE creating prompt file at: $prompt_file"
 create_yaml
 create_prompt
 
+echo ""
 echo "RUNNING AlpacaEval... "
 for lang in "${lang_codes[@]}"; do
   job_id=$(sbatch --job-name="alpaca_${lang}" --export=model_name=$model_name,lang=$lang launch_scripts/eval_from_model.sh | awk '{print $4}')
